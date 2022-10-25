04156e9f9cdfedb4dd087510bd7c0b5b70b874bc
dothanhdat0907 <75473674+dothanhdat0907@users.noreply.github.com>
2022-10-25 21:27:58 +0700

    first commit

---
+++ frontend/src/components/Login/Login.jsx
@@ -0,0 +1,9 @@
+import login_img from '../../assets/login.png'
+import "./Login.scss"
+export const Login = () => {
+    return (
+        <div className='img'>
+            <img className='login_img' src={login_img}/>
+        </div>
+    )
+}
