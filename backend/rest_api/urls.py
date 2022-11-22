ea5c3e732f57ad946e073b1bdc7a2ce5b920cf10
mrgThang <uetbvnu@gmail.com>
2022-11-22 22:58:27 +0700

    response cv as zip

--- backend/rest_api/urls.py
+++ backend/rest_api/urls.py
@@ -28,11 +28,24 @@ urlpatterns = [
     path('accounts/', account_views.account_list),
     path('accounts/<int:id>', account_views.account_detail),
     path('recruitments/', recruitment_views.recruitment_list),
+
+    #trả về recruitment dựa theo id recruitment + update recruitment dựa trên id
     path('recruitments/<int:id>', recruitment_views.recruitment_detail),
+
+    #trả về recruitment dựa trên tham số job và salary
     path('search/', recruitment_views.search),
+
+    #trả về recruitment dựa trên idCompany
+    path('search/<int:idCompany>', recruitment_views.recruitments_of_company),
+
     path('cvs', cv_views.cv_list),
     path('cvs/<int:id>', cv_views.cv_detail),
+
+    #trả về cv dựa trên id
     path('get-cv/<int:id>', cv_views.cv_getfile),
+
+    #trả về cvs dựa trên idRecruitments
+    path('cvs-of-recruitment/<int:idRecruitment>', cv_views.cv_of_recruitment),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
